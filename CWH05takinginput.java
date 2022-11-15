a1fa59c0ccf92a4adc26b6202d865dd7b492c730
AbhishekBihar <92347144+AbhishekBihar@users.noreply.github.com>
2022-11-15 23:47:24 +0530

    Add files via upload

---
+++ CWH05takinginput.java
@@ -0,0 +1,12 @@
+//import java.util;
+public class CWH05takinginput {
+    public static void main(String[] args) {
+        System.out.println("it works");
+        int age=32;
+        int beg=52;
+        int sum=age+beg;
+       // byte age=34;
+       // char A='b';
+        System.out.println(sum);
+    }
+}
