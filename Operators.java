bae774c3f3fdea1ecd72a94f9771bb36caa50c7c
AbhishekBihar <92347144+AbhishekBihar@users.noreply.github.com>
2022-11-16 23:44:01 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,9 @@
+public class Operators {
+    public static void main(String[] args) {
+        int a=5;
+        int b=8;
+        b += 2;
+        System.out.println(b);
+        System.out.println(64<98);
+    }
+}
