50cbd4b0dce096dd3be3179e4c84ab34dc12bce7
AbhishekBihar <92347144+AbhishekBihar@users.noreply.github.com>
2022-11-16 23:47:10 +0530

    Add files via upload

---
+++ Subtract.java
@@ -0,0 +1,10 @@
+public class Subtract {
+        public static void main(String[] args) {
+            int a=23;
+            int b=23;
+            int sum=a+b;
+            System.out.println(sum);
+
+        }
+
+    }
