f0ef51a18b701c3a6f9b1ca9d5896867d755d886
AbhishekBihar <92347144+AbhishekBihar@users.noreply.github.com>
2022-11-17 23:41:35 +0530

    Add files via upload

---
+++ Resulting_datatype.java
@@ -0,0 +1,7 @@
+public class Resulting_datatype {
+    public static void main(String[] args) {
+        int a= 23;
+        float b=23.12f + a;  //SUM OF FLOAT AND INT IS ALWAYS FLOAT
+        System.out.println(b);
+    }
+}
