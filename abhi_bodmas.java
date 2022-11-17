44bfed0a3804c80b706b5307b48e13b14d1597ff
AbhishekBihar <92347144+AbhishekBihar@users.noreply.github.com>
2022-11-17 23:11:34 +0530

    Add files via upload

---
+++ abhi_bodmas.java
@@ -0,0 +1,8 @@
+public class abhi_bodmas {
+    public static void main(String[] args) {
+        int a= 32*2-49/7;
+        System.out.println("Answer is :" + a);
+        int b= 32/2+14*6754+12/5*5;
+        System.out.println("Answer is: " + b);
+    }
+}
