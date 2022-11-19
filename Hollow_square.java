dd7abcec16662c214da3a38692c0f74269b27c11
AbhishekBihar <92347144+AbhishekBihar@users.noreply.github.com>
2022-11-19 21:10:38 +0530

    Add files via upload

---
+++ Hollow_square.java
@@ -0,0 +1,18 @@
+import org.w3c.dom.ls.LSOutput;
+
+/*public class Hollow_square {
+    public static void main(String[] args) {
+        //int = i,j;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i==0||i==4||j==0||j==4)
+                    System.out.print("*");
+            }
+            //else
+            System.out.print(" ");
+
+        }
+
+    }
+    System.out.println ();
+}*/
