c9dd183e5e33679b8eb6ce2d791917cecd3af8cf
AbhishekBihar <92347144+AbhishekBihar@users.noreply.github.com>
2022-11-20 22:11:03 +0530

    Add files via upload

---
+++ Abhi_Typecasting.java
@@ -0,0 +1,11 @@
+public class Abhi_Typecasting {
+    public static void main(String[] args) {
+        char grade= 'B';
+        grade= (char)(grade+8);
+        System.out.println(grade);
+
+        //decoding for understanding for friend
+        grade= (char)(grade-8);
+        System.out.println(grade);
+    }
+}
